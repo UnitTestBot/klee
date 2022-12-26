4c8c6f2713134c10b8f05262967842bccfcbc775
Alex Babushkin <ocelaiwo@gmail.com>
2022-12-26 15:46:53 +0500

    [tempfix] Write addresses of MOs in KTest

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -5136,6 +5136,7 @@ bool Executor::getSymbolicSolution(const ExecutionState &state,
     auto mo = state.symbolics[i].first;
     KTestObject *o = &res.objects[i];
     o->name = const_cast<char*>(mo->name.c_str());
+    o->address = mo->address;
     o->numBytes = values[i].size();
     o->bytes = new unsigned char[o->numBytes];
     std::copy(values[i].begin(), values[i].end(), o->bytes);
